f7f9b9049a45e7653b537eb25fbf4f99b4a81d99
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-07-20 13:54:29 +0530

    Day1.js

---
+++ js/Day1.js
@@ -0,0 +1,157 @@
+// Question 1 : Guess the output. 
+
+// function aboutVar(){
+//     var a = 1; 
+//     function inner(){
+//         var a  = 2
+//         console.log(a);
+//         console.log(a);
+//     }
+//     inner()
+//     console.log(a); 
+// }
+// aboutVar(); 
+// console.log(a); 
+
+
+
+// Question 2 :  Guess the output. 
+
+// var a = "nisha"; 
+// var a = 20; 
+// console.log(a);
+// console.log(a);
+
+
+// Question 3 :  Guess the output. 
+
+// var a = 20; 
+// function aboutVar10(){
+//     var a = "mern"
+//     console.log(a); 
+// }
+// console.log(a); 
+// aboutVar10(); 
+// console.log(a); 
+
+
+// Question 4 :  Guess the output. 
+
+// aboutVar11();
+// var a = 20; 
+// function aboutVar11(){
+//     var a = 'mern'; 
+//     console.log(a);
+// }
+// console.log(a);
+// console.log(a);
+
+
+// Question 5 :  Guess the output. 
+
+// var a = 1; 
+// if(a == true){
+//     console.log(a);
+// }
+// console.log(a);
+
+
+
+// Var hoisting   ----- we can redefine var ---- funtional scope
+
+// var a = 1; 
+// console.log(a);
+
+
+
+// console.log(a);
+// var a = 1; 
+
+
+
+
+// let  ----- we can't redefine let ----- block scope
+// we can update let.
+
+// let a; 
+// console.log(a);
+
+// let a = 1; 
+// console.log(a);
+
+
+// console.log(a);
+// let a = "nisha"; 
+
+
+
+// aboutLet4()
+// console.log(a); 
+
+// let a = 1; 
+// function aboutLet4(){
+//     let a = 34; 
+//     console.log(a);
+// }
+// aboutLet4()
+
+
+// let a = 1; 
+// function aboutLet5(){
+//     console.log(a); 
+
+//     let a = 34; 
+//     console.log(a);
+// }
+// aboutLet5()
+
+
+
+
+
+//const : -- block scope -- can't update --- 
+
+// const a; 
+// let b; 
+// console.log(a);
+// console.log(b);
+
+
+// const c = 1;
+// c = 2; 
+// console.log(c);
+
+
+// let a = "Hi, This is parul uni"
+// console.log("Good ${a}");
+
+
+// let b = "hi"
+
+// let a = `hi
+// hello
+// namste`
+// console.log(a);
+
+
+// let a = "morning"
+// console.log(`Good ${a}`);
+
+
+//####################################################################
+
+// let name1 = 'riya'; 
+// let name2 = 'riya'; 
+// let name3 = 'riya'; 
+
+
+// let a = "Mern"; 
+// a[0] = "p"; 
+// console.log(a);
+
+// a = "Pern"
+// console.log(a.length);
+// console.log(a);
+
+
+
