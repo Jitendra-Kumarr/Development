579ae94c85eee9702fb125e7c4331650aa8052a8
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-07-21 15:19:46 +0530

    Day2

---
+++ js/Day2.js
@@ -0,0 +1,67 @@
+
+
+// slicing. 
+
+// var str = "Jitendra"; 
+// console.log(str.slice(0,5));
+
+
+
+
+// split
+
+// var str = "Jitendra"; 
+// var newStr = str.split('')
+
+
+// for(var i=0; i<newStr.length; i++){
+//     newStr[i] = newStr[i] + ",";
+// }
+
+// var s = "";
+// for(var i=0; i<newStr.length; i++){
+//     s += newStr[i];
+// }
+// console.log(s)
+
+
+
+
+//trim
+
+// var str = " Jitendra     "; 
+// console.log(str.trim()); 
+
+
+
+
+// replace(old, new)
+// var str = "Hello World"; 
+// var newStr = str.replace("Hello", "namaste"); 
+
+// console.log(newStr);
+
+
+// includes 
+// const str = "Hello World"; 
+// if(str.includes("World")){
+//     console.log("it's Includes! "); 
+// }
+
+
+// uppercase and lowercase
+// const str = "String"; 
+// const upper = str.toUpperCase()
+// const lower = str.toLowerCase()
+// console.log(lower);
+
+
+//indexof 
+// const str = "String"; 
+// console.log(str.indexOf("i"));
+
+
+
+const str = "String"; 
+
+
