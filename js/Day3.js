d77b21f1542c60e3ffd0eb400075b34d8478b349
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-07-22 14:09:14 +0530

    Day3

---
+++ js/Day3.js
@@ -0,0 +1,69 @@
+// LOOPS 
+
+
+
+//function scope
+
+// function aboutVar(){
+//     var a = 10;
+//     console.log(a);
+// }
+// aboutVar(); //10
+// console.log(a) //reference error
+
+
+
+//block scope
+
+// if(true){
+//     let c = 2;
+//     const b = 3;
+//     var a = 5; 
+// }
+// console.log(a);
+// console.log(b); // reference error
+// console.log(c); // reference error
+
+
+//  truthy value and falsy value 
+
+// falsy value -> false, 0, "", undefined, NaN, null ------------  expect this all are truthy values 
+
+
+// let username = []; 
+// let len = username.length
+
+// if(username){
+//     console.log("Truthy") // prints this
+// } else {
+//     console.log("falsy")
+// }
+
+
+
+// for in loop 
+// obj = {
+//     Name : "Jitendra", 
+//     Batch : 4, 
+//     id : 2
+// }
+
+// for(let key in obj){
+//     console.log(key)
+// }
+
+
+
+// for-off loop  
+// let arr = [1, "Nisha", true];
+
+// for(let i of arr){
+//     console.log(i); 
+// }
+
+
+
+
+
+
+
