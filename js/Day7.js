c2d1f96305e38b78d0c2d3be0bd7225b02852814
Jitendra Kumar <kr.jitendra7979@gmail.com>
2026-08-04 10:27:34 +0530

    Day7

---
+++ js/Day7.js
@@ -0,0 +1,56 @@
+// const heading = document.getElementById("main");
+// console.log(heading);
+
+// const h1 = document.getElementById("main")
+// // console.log(h1);
+// console.log(h1.textContent);
+// h1.textContent = "Namste Student"
+// console.log(h1.textContent);
+
+
+// const comment = "<img src=x onerror='alert(document.cookie)'>";
+// commentBox.innerHTML = comment; 
+
+// const h1Query = document.querySelector("h1")
+// console.log(h1Query);
+
+// const h1Queryy = document.querySelector("#main")
+// console.log(h1Queryy);
+
+// const classs = document.getElementsByClassName("para2")
+// console.log(classs)
+
+// const query = document.querySelector("#fruit")
+// console.log(query)
+
+// const multiple = document.querySelectorAll("#main, .para1, ul")
+// console.log(multiple);
+// console.log(multiple.textContent);
+
+
+
+
+
+
+// const heading = document.getElementById("main");
+// console.log(heading);
+// console.log(heading.textContent);
+
+// heading.textContent = "Javascript"
+
+// console.log(heading.innerHTML);
+// heading.innerHTML = "<h2>Heading 2</h2>"
+
+
+
+let input = document.getElementById("input")
+// console.log(input.value)
+// console.log(input.disabled)
+// console.log(input.type)
+
+input.value = "output"; 
+input.disabled = false; 
+console.log(input.disabled)
+
+console.log(input.getAttribute('value'))
+console.log(input.value)
